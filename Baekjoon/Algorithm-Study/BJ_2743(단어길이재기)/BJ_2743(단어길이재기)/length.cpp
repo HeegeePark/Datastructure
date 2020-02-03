c292a2405d21655117e094940479161e4b7f0ec0
HeegeePark <phg980513@naver.com>
2020-02-03 11:48:51 +0900

    solve 2743

---
+++ Baekjoon/Algorithm-Study/BJ_2743(단어길이재기)/BJ_2743(단어길이재기)/length.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+
+int main() {
+	string S;
+
+	getline(cin, S);
+	cout << S.length() << endl;
+	
+	//system("pause");
+	return 0;
+}
