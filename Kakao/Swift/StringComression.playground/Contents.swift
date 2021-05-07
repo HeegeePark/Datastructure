dcc2ea6c6e614ec2405e2c03dc1ef7cb45766186
heegeepark <phg980513@naver.com>
2021-05-08 00:30:37 +0900

    Feat: Solve Kakao 2020 카카오 공채 문자열 압출 풀이

--- Kakao/Swift/StringComression.playground/Contents.swift
+++ Kakao/Swift/StringComression.playground/Contents.swift
@@ -1,6 +1,8 @@
+// 2020 카카오 채용 문자열 압축
 import Foundation
 
 func solution(_ s:String) -> Int {
+    var stringCopy = s
     var answer = s.count
     
     for i in 1..<((s.count / 2) + 1) {
@@ -8,14 +10,18 @@ func solution(_ s:String) -> Int {
         var pos = 0
         
         while pos + i <= s.count {
-            let unit = s[pos...pos + i]
-            pos += 1
+            let start = stringCopy.index(stringCopy.startIndex, offsetBy: pos)
+            let end = stringCopy.index(stringCopy.startIndex, offsetBy: pos + i)
+            let unit = stringCopy[start..<end]
+            pos += i
             
             var cnt = 0
             while pos + i <= s.count {      // 중복되는지 check
-                if unit == s[pos...pos + i] {
+                let start = stringCopy.index(stringCopy.startIndex, offsetBy: pos)
+                let end = stringCopy.index(stringCopy.startIndex, offsetBy: pos + i)
+                if unit == stringCopy[start..<end] {
                     cnt += 1
-                    pos += 1
+                    pos += i
                 } else { break }
             }
             
@@ -23,13 +29,13 @@ func solution(_ s:String) -> Int {
                 len = len - i * cnt
                 switch cnt {
                     case 1..<9:
-                    len += 1
+                        len += 1
                     case 9..<99:
-                    len += 2
+                        len += 2
                     case 99..<999:
-                    len += 3
-                    case 999..<9999:
-                    len += 4
+                        len += 3
+                    default:
+                        len += 4
                 }
             }
         }
