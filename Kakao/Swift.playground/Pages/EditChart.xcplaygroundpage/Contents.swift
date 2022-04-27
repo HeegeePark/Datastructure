c2afb3f0a352f1df96fab9b004a108f3fe88a1dc
heegeepark <phg980513@naver.com>
2022-04-27 23:34:45 +0900

    Feat: Solve Kakao - 2021 카카오 인턴십 표 편집 풀이

--- Kakao/Swift.playground/Pages/EditChart.xcplaygroundpage/Contents.swift
+++ Kakao/Swift.playground/Pages/EditChart.xcplaygroundpage/Contents.swift
@@ -5,64 +5,59 @@
 import Foundation
 
 class Node {
-    var isRemoved: Bool
+    var idx = 0
     var prev: Node?
     var next: Node?
-    
-    init() {
-        self.isRemoved = false
-        self.prev = nil
-        self.next = nil
-    }
 }
 
 func solution(_ n:Int, _ k:Int, _ cmd:[String]) -> String {
-    let nodeArr = Array(repeating: Node(), count: n)    // 삭제 여부를 담는 노드 배열
-    var selected: Node = nodeArr[k]     // 현재 선택 행
+    var node = Node()
+    var selected = node     // 현재 선택 행
     var deletedStack = [Node]() // 삭제된 행을 담는 스택 휴지통
-    var answer = ""
+    var answer = [String](repeating: "O", count: n)
     
     // 링크드 리스트 연결하기
     for i in 1..<n {
-        nodeArr[i].prev = nodeArr[i-1]
-        nodeArr[i-1].next = nodeArr[i]
+        node.next = Node()
+        node.next?.prev = node
+        node.next?.idx = i
+        if i == k { selected = node.next! }
+        node = node.next!
     }
     
     // 명령어 실행
     cmd.forEach {
         let c = $0.first!
         switch c {
-            case "U":
-            let move = $0.last!.wholeNumberValue!
+        case "U":       // "U X"
+            let move = Int($0.split(separator: " ").map { String($0) }[1])!
             for _ in 0..<move { selected = selected.prev! }
-            case "D":
-            let move = $0.last!.wholeNumberValue!
+        case "D":       // // "D X"
+            let move = Int($0.split(separator: " ").map { String($0) }[1])!
             for _ in 0..<move { selected = selected.next! }
-            case "C":
+        case "C":       // "C"
             deletedStack.append(selected)
-            selected.isRemoved.toggle()
+            answer[selected.idx] = "X"
             let up = selected.prev
             let down = selected.next
-            if up != nil { up!.next = down }
-            if down != nil {
-                down!.prev = up
-                selected = down!
-            } else {
+            up?.next = down
+            guard down != nil else {
                 selected = up!
                 return
             }
-            default:
-            let node = deletedStack.removeLast()
-            node.isRemoved.toggle()
-            let up = node.prev
-            let down = node.next
-            if up != nil { up!.next  = node }
-            if down != nil { down!.prev  = node }
+            down!.prev = up
+            selected = down!
+        default:        // "Z"
+            let restore = deletedStack.removeLast()
+            answer[restore.idx] = "O"
+            let up = restore.prev
+            let down = restore.next
+            up?.next = restore
+            down?.prev = restore
         }
     }
     
-    nodeArr.forEach { answer += !$0.isRemoved ? "O": "X" }
-    return answer
+    return answer.joined()
 }
 
 solution(8, 2, ["D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z"])
