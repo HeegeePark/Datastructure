0c795e020529ccc56162ba98368c58c25f3031b6
heegeepark <phg980513@naver.com>
2022-06-28 23:56:43 +0900

    Feat: Update BJ 3040 더 정리

--- Baekjoon/DynamicPrograming/BJ_3687(성냥개비)/BJ_3687(성냥개비)/main.swift
+++ Baekjoon/DynamicPrograming/BJ_3687(성냥개비)/BJ_3687(성냥개비)/main.swift
@@ -7,5 +7,18 @@
 
 import Foundation
 
-print("Hello, World!")
+let N = Int(readLine()!)!
+//var inputs = [Int]()
+var input = 0
+
+for _ in 0..<N {
+//    inputs += [Int(readLine()!)!]
+    input = Int(readLine()!)!
+    
+}
+
+func solution(_ x: Int) {
+    var dict: [Int: Int] = [1: 2, 2: 5, 3: 5, 4: 4, 5: 5, 6: 6, 7: 3, 8: 7, 9: 6, 0: 6]
+    dict.sorted { $0.value < $1.value }
+}
 
--- Kakao/Swift.playground/Pages/양궁대회.xcplaygroundpage/Contents.swift
+++ Kakao/Swift.playground/Pages/양궁대회.xcplaygroundpage/Contents.swift
@@ -5,16 +5,16 @@ import Foundation
 
 var N = 0
 var apeach = [Int]()
-var needs = [Int]()  // 점수 별 획득하기 위한 최소 화살 개수
-var scores = [Int: [Int]]()   // [점수 차: 라이언 과녁 정보]
+var needs = [Int](repeating:0, count: 11)  // 점수 별 획득하기 위한 최소 화살 개수
+var scores = [[Int]: Int]()   // [라이언 과녁 정보: 최종점수 차]
 
 // 라이언과 어피치 점수 차 return
 func getScores(_ ryan: [Int]) -> Int {
-    var scores = [0, 0] // [라이언 점수, 어피치 점수]
+    var scores = [0, 0] // [라이언 최종점수, 어피치 최종점수]
     
-    for i in 0...10 {
+    for i in 0..<10 {   // 과녁 밖(10번째)는 득점이 없으니 돌지 않음
         if ryan[i] > apeach[i] {
-            scores[0] += (10 - i)
+            scores[0] += 10 - i
         } else {
             scores[1] += apeach[i] > 0 ? 10 - i: 0
         }
@@ -22,17 +22,21 @@ func getScores(_ ryan: [Int]) -> Int {
     return scores[0] - scores[1]
 }
 
-
 func dfs(_ curr: Int, _ count: Int, _ comb: [Int]) {
-    if count == N {
-        if getScores(comb) > 0 {
-            scores[getScores(comb)] = comb
+    if getScores(comb) > 0 {
+        // 화살을 다 쏘지 않아도, 어피치를 이긴다면 남은 화살은 0점 과녁으로
+        if count < N {
+            var newComb = comb
+            newComb[10] = N - count
+            scores[newComb] = getScores(comb)
+        } else {
+            scores[comb] = getScores(comb)
             return
-        } else { return }
+        }
     }
-    if curr >= 10 { return }
+    if curr >= 10 || count > N { return }
     
-    for i in curr + 1...10 {
+    for i in curr + 1..<needs.count {
         if count + needs[i] <= N {
             var newComb = comb
             newComb[i] = needs[i]
@@ -42,22 +46,38 @@ func dfs(_ curr: Int, _ count: Int, _ comb: [Int]) {
 }
 
 func solution(_ n:Int, _ info:[Int]) -> [Int] {
-    if n == 1 { return [-1] }
+    if n == 1  && info[0] == 1 { return [-1] }
     N = n
     apeach = info
-    info.forEach {
-        needs.append($0 + 1)
+    info.enumerated().forEach {
+        needs[$0.0] = $0.1 + 1
+    }
+    for i in 0..<10 {
+        needs[i] = info[i] + 1
     }
     
     // dfs로 라이언이 이길 수 화살 조합 찾기
     for i in 0...10 {
-        let comb = [Int](repeating: 0, count: 11)
+        var comb = [Int](repeating: 0, count: 11)
+        comb[i] = needs[i]
         dfs(i, needs[i], comb)
     }
     
-    return scores.isEmpty ? [-1]: scores.max { $0.key < $1.key }!.value
+    guard !scores.isEmpty else { return [-1] }
+    return scores.sorted() {
+        if $0.value == $1.value {   // 점수 차가 같을 경우, 가장 낮은 점수 비중이 많은 과녁 정보 return
+            let n1 = Int($0.key.reversed().map { String($0) }.joined())!
+            let n2 = Int($1.key.reversed().map { String($0) }.joined())!
+            return n1 <= n2
+        } else {
+            return $0.value < $1.value
+        }
+    }.last!.key
 }
 
 solution(5, [2,1,1,1,0,0,0,0,0,0,0])
+//solution(1, [1,0,0,0,0,0,0,0,0,0,0])
+//solution(9, [0,0,1,2,0,1,1,1,1,1,1])
+//solution(10, [0,0,0,0,0,0,0,0,3,4,3])
 
 //: [Next](@next)
