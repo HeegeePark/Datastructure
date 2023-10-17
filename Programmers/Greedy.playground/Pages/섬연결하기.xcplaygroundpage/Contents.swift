9c2816df1e0ab203375b4378a70112b2f570e46e
kizi <phg980513@naver.com>
2023-10-18 00:59:52 +0900

    Fix: 섬 연결하기(프머_그리디) 최종 코드 수정
    - 크루스칼 알고리즘

--- Programmers/Greedy.playground/Pages/섬연결하기.xcplaygroundpage/Contents.swift
+++ Programmers/Greedy.playground/Pages/섬연결하기.xcplaygroundpage/Contents.swift
@@ -3,48 +3,51 @@
 import Foundation
 
 func solution(_ n:Int, _ costs:[[Int]]) -> Int {
-    var graph: [Int: [(key: Int, value: Int)]] = [:]
-    var answer = Int.max
+    var parents: [Int] = Array(0...n-1)
+    let costs = costs.sorted { $0[2] < $1[2] }
     
-    for info in costs {
-        graph[info[0], default: []] += [(info[1], info[2])]
-        graph[info[1], default: []] += [(info[0], info[2])]
-    }
-    
-    func bfs(_ start: Int, _ visited: inout [Bool]) -> Int {
-        var q = [Int]()
-        q.append(start)
-        var cost = 0
+    // 최상위 부모 등록
+    func unionSet(_ start: Int, _ end: Int) {
+        var start = start
+        var end = end
         
-        var idx = 0
-        while idx < q.count {
-            let curr = q[idx]
-            print("curr", curr)
-            visited[curr] = true
-            idx += 1
-            
-            guard idx < n else { break }
-            
-            guard let nodes = graph[curr] else { continue }
-            
-            var temp = (key: -1, value: Int.max)
-            for node in nodes {
-                if !visited[node.key] && node.value < temp.value {
-                    print(node)
-                    temp = (node.key, node.value)
-                }
-            }
-            cost += temp.value
-            q.append(temp.key)
+        start = findSet(start)
+        end = findSet(end)
+        if start != end {
+            parents[end] = start
         }
-        print(cost)
-        return cost
     }
     
-    for i in 0..<n {
-        var visited = Array(repeating: false, count: n)
-        print("----", i)
-        answer = min(answer, bfs(i, &visited))
+    // 최상위 부모 찾기
+    func findSet(_ start: Int) -> Int {
+        if parents[start] == start {
+            return start
+        }
+        
+        // 자신이 최상위 부모가 아니라면 재귀로 찾으러
+        let parent = findSet(parents[start])
+        parents[start] = parent
+        
+        // 자신을 부모로 가지는 노드들의 부모 업뎃
+        for i in 0..<parents.count {
+            if parents[i] == start {
+                parents[i] = parent
+            }
+        }
+        
+        return parent
+    }
+    
+    var answer = 0
+    for cost in costs {
+        let start = findSet(cost[0])
+        let end = findSet(cost[1])
+        let value = cost[2]
+        
+        if start != end {
+            answer += value
+            unionSet(start, end)
+        }
     }
     
     return answer
